d7d3e9881e343d1a4ffa52dc18d6af364b58883f
bolsog <>
2004-11-14 14:35:00 +0000

    slackware package build added

---
+++ imskpe/packages/makepkg.sh
@@ -0,0 +1,19 @@
+#
+# make slackware package
+#
+
+#!/bin/sh
+
+VERSION=1.0beta1
+TMP=/tmp
+
+cp imskpe.desc $TMP/imskpe-$VERSION-i686-bol.txt
+cd ..
+make clean
+sudo rm -rf $TMP/pkg-imskpe
+./configure --prefix=/usr/local
+make
+sudo make install-strip DESTDIR=$TMP/pkg-imskpe
+cd $TMP/pkg-imskpe
+tar cfz ../imskpe-$VERSION-i686-bol.tgz .
+
