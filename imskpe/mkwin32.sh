44814d1d6bd5957849e4eb0866e94516354aab20
bolsog <>
2010-05-02 16:22:32 +0000

    fixes for building win32-binaries and ChangeLog for 1.0.2 release

--- imskpe/mkwin32.sh
+++ imskpe/mkwin32.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 
-echo "this is for crosscompiling an win32-exe under MY linux."
+# install mingw32 for ubuntu:
+# sudo apt-get install mingw32
+
+echo "this is for crosscompiling an win32-exe. Only tested on MY Ubuntu 9.10!!"
 echo
 
 make clean
 rm -f src/imskpe.exe
 
-PKG_CONFIG_PATH="~/local/cross/lib/pkgconfig" ./configure --host=i386-mingw32 --without-libtoolfix   --with-pthreads=no  --with-win32 CC=~/local/cross/bin/i386-mingw32msvc-gcc LD=~/local/cross/bin/i386-mingw32msvc-ld RANLIB=~/local/cross/bin/i386-mingw32msvc-ranlib
+./configure --with-win32
+export CC=i586-mingw32msvc-gcc
+export CXX=i586-mingw32msvc-g++
+export LD=i586-mingw32msvc-ld
+export AR=i586-mingw32msvc-ar
+export AS=i586-mingw32msvc-as
+export NM=i586-mingw32msvc-nm
+export STRIP=i586-mingw32msvc-strip
+export RANLIB=i586-mingw32msvc-ranlib
+export DLLTOOL=i586-mingw32msvc-dlltool
+export OBJDUMP=i586-mingw32msvc-objdump
+export RC=i586-mingw32msvc-windres
+export PKG_CONFIG_PATH=~/cross/lib/pkgconfig
 
 make
 
